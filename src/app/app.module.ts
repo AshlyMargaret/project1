0cb5acb4176114fc10e92de2ad3550e932c0d3e4
Ashly Margaret <ashlymargaret030@gmail.com>
2023-01-09 16:47:58 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
